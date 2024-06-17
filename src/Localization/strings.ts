8f51aa94f233e7e59aec691b9aa5a73f59432534
Sergei Bogachev <sergeyvbo@gmail.com>
2024-06-16 21:17:48 -0700

    add localized strings

---
+++ src/Localization/strings.ts
@@ -0,0 +1,24 @@
+const strings = {
+    en: {
+        settings: {
+            language: "Language",
+            showPin: "Show pin for small countries",
+            difficulty: "Difficulty",
+            easy: "Easy",
+            medium: "Medium",
+            hard: "Hard",
+            showZoomButtons: "Show zoom buttons",
+        } as const,
+    } as const,
+    ru: {
+        settings: {
+            language: "Язык",
+            showPin: "Показывать метку для маленьких стран",
+            difficulty: "Уровень сложности",
+            easy: "Легко",
+            medium: "Средне",
+            hard: "Сложно",
+            showZoomButtons: "Показывать кнопки масштаба",
+        } as const,
+    } as const,
+} as const
